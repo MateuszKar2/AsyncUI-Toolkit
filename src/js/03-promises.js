7ede60c46d40f4cccf27be43a38ddc0dd8f2dd14
MateuszKar2 <system505@protonmail.com>
2023-04-06 20:34:11 +0200

    add third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import Notiflix from 'notiflix';
+
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const button = document.querySelector('button[type="submit"]');
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  })
+  return promise;
 }
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  for (let i = 0; i < amount.value; i++) {
+    createPromise(1 + i, Number(delay.value) + Number(step.value * i))
+
+      .then(({ position, delay }) => {
+        Notiflix.Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.info(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+})
