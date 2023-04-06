4e64ca91e1e22a6b89591d8aec3690a0f3d309ae
MateuszKar2 <system505@protonmail.com>
2023-04-06 21:40:09 +0200

    improve task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,19 @@
+//W HTML znajduje się znacznik formularza, w którego pola użytkownik będzie wprowadzał pierwsze opóźnienie w milisekundach, 
+//stopień zwiększenia opóźnienia dla każdej kolejnej obietnicy i liczbę obietnic, które należy utworzyć.
+//napisz skrypt który po wysłaniu formularza wywoła f.createPromise tyle razy ile wprowadzone w pole amount
+//Po każdym wywołaniu przekaż jej numer utworzonej obietnicy (position) i opóźnienie, 
+//uwzględniając wprowadzone przez użytkownika pierwsze opóźnienie (delay) i stopień (step).
+//Uzupełnij kod funkcji createPromise tak, aby przywracała jedną obietnicę, która wykonuje się przez delay czasu. 
+//Wartością obietnicy powinien być obiekt, w którym będą właściwości position i delay z wartościami parametrów o tej samej nazwie.
+//Użyj kodu początkowego funkcji, aby wybrać to, co należy zrobić z obietnicą - resolve lub reject.
+//------------------
+//Korzystam z biblioteki Notiflix: to czysta biblioteka JavaScript do nieblokujących powiadomień po stronie klienta,
+//wyskakujących okienek, wskaźników ładowania i innych elementów
+//Korzystając z podanego szkieletu f. createPromise modyfikuje ją zgodnie z zadaniem
+//Dodaje detektor zdarzeń .addEventListener do button
+//Anuluje domyślne działanie przeglądarki za pomocą metody .preventDefault()
+//Korzystam z pętli for aby uzupełnić podaną f.createPromise w zadaniu
+
 import Notiflix from 'notiflix';
 
 const delay = document.querySelector('input[name="delay"]');
